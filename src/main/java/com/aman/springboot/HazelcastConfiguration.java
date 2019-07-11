61359612115fdb9344a9e3cf4c68253e789efad7
GARG Aman <g522074@mph.morpho.com>
2019-07-11 15:39:02 +0530

    Initial Commit

---
+++ src/main/java/com/aman/springboot/HazelcastConfiguration.java
@@ -0,0 +1,32 @@
+package com.aman.springboot;
+
+import com.hazelcast.config.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HazelcastConfiguration {
+
+    @Bean
+    public Config hazelCastConfig(){
+        Config config =new Config()
+                .setInstanceName("hazelcast-instance")
+                .addMapConfig(
+                        new MapConfig()
+                                .setName("instruments")
+                                .setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
+                                .setEvictionPolicy(EvictionPolicy.LRU)
+                                .setTimeToLiveSeconds(20));
+        NetworkConfig network = config.getNetworkConfig();
+        network.setPort(5701).setPortCount(20);
+        network.setPortAutoIncrement(true);
+
+        JoinConfig join = network.getJoin();
+        join.getMulticastConfig().setEnabled(false);
+        join.getTcpIpConfig()
+                .addMember("192.168.21.223")
+                .addMember("192.168.20.71").setEnabled(true);
+        return config;
+    }
+
+}
