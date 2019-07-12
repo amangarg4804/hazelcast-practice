86605f191bf4305fdc5a96d484adf4470f0685fd
GARG Aman <g522074@mph.morpho.com>
2019-07-12 16:37:39 +0530

    Management center config

--- src/main/java/com/aman/springboot/HazelcastConfiguration.java
+++ src/main/java/com/aman/springboot/HazelcastConfiguration.java
@@ -8,15 +8,16 @@ import org.springframework.context.annotation.Configuration;
 public class HazelcastConfiguration {
 
     @Bean
-    public Config hazelCastConfig(){
-        Config config =new Config()
+    public Config hazelCastConfig() {
+        Config config = new Config()
                 .setInstanceName("hazelcast-instance")
                 .addMapConfig(
                         new MapConfig()
                                 .setName("instruments")
                                 .setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
-                                .setEvictionPolicy(EvictionPolicy.LRU)
-                                .setTimeToLiveSeconds(20));
+                                .setEvictionPolicy(EvictionPolicy.LRU));
+        config.getManagementCenterConfig().setEnabled(true);
+        config.getManagementCenterConfig().setUrl("http://192.168.20.71:8081/hazelcast-mancenter");
         NetworkConfig network = config.getNetworkConfig();
         network.setPort(5701).setPortCount(20);
         network.setPortAutoIncrement(true);
